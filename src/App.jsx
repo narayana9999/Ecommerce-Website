14d82a01032d0d9f7f19881a368aad156ebbd7f1
Lakshmi Narayana <63528587+narayana9999@users.noreply.github.com>
2022-11-05 21:15:42 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Home from "./pages/home";
+//import Product from "./pages/Product";
+//import ProductList from "./pages/ProductList";
+//import Register from "./pages/Register";
+//import Login from "./pages/Login";
+//import Cart from "./pages/Cart";
+const App = () => {
+  return <Home/>;
+};
+
+export default App;
